e6f4fb79853b958c8249801e94b01f79b1e5b229
Paco Ho <kaiipho@gmail.com>
2024-08-06 17:01:00 +0800

    change str2Int function to accept abstractstring

--- src/CommonTools.jl
+++ src/CommonTools.jl
@@ -2,6 +2,6 @@ module CommonTools
 
 export str2Int
 
-str2Int(str::String) = str == "" ? 0 : parse(Int, str)
+str2Int(str<:AbstractString) = str == "" ? 0 : parse(Int, str)
 
 end # module CommonTools
